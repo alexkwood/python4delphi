dafce9dbb1488bb5480202df295437a2c7bcfc77
hartmutdavid <info@hadv.de>
2017-12-14 13:23:47 +0100

    This version includes customizations to Python 3.x, 64bit and FireDAC.

---
+++ clean.bat
@@ -0,0 +1,145 @@
+rd /s /q PythonForDelphi\Components\__history
+rd /s /q PythonForDelphi\Components\bpl
+rd /s /q PythonForDelphi\Components\dcp
+rd /s /q PythonForDelphi\Components\dcu
+
+rd /s /q PythonForDelphi\Components\Sources\Core\__history
+rd /s /q PythonForDelphi\Components\Sources\Core\__recovery
+
+rd /s /q PythonForDelphi\Components\Sources\FireDAC\__history
+rd /s /q PythonForDelphi\Components\Sources\FireDAC\__recovery
+
+rd /s /q PythonForDelphi\Demos\Demo01\__history
+rd /s /q PythonForDelphi\Demos\Demo01\__recovery
+rd /s /q PythonForDelphi\Demos\Demo01\Win64
+del /q PythonForDelphi\Demos\Demo01\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo01\Project1.identcache
+del /q PythonForDelphi\Demos\Demo01\Project1.stat
+del /q PythonForDelphi\Demos\Demo01\Project1.exe
+del /q PythonForDelphi\Demos\Demo01\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo02\__history
+rd /s /q PythonForDelphi\Demos\Demo02\__recovery
+rd /s /q PythonForDelphi\Demos\Demo02\Win64
+del /q PythonForDelphi\Demos\Demo02\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo02\Project1.identcache
+del /q PythonForDelphi\Demos\Demo02\Project1.stat
+del /q PythonForDelphi\Demos\Demo02\Project1.exe
+del /q PythonForDelphi\Demos\Demo02\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo03\__history
+rd /s /q PythonForDelphi\Demos\Demo03\__recovery
+rd /s /q PythonForDelphi\Demos\Demo03\Win64
+del /q PythonForDelphi\Demos\Demo03\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo03\Project1.identcache
+del /q PythonForDelphi\Demos\Demo03\Project1.stat
+del /q PythonForDelphi\Demos\Demo03\Project1.exe
+del /q PythonForDelphi\Demos\Demo03\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo04\__history
+rd /s /q PythonForDelphi\Demos\Demo04\__recovery
+rd /s /q PythonForDelphi\Demos\Demo04\Win64
+del /q PythonForDelphi\Demos\Demo04\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo04\Project1.identcache
+del /q PythonForDelphi\Demos\Demo04\Project1.stat
+del /q PythonForDelphi\Demos\Demo04\Project1.exe
+del /q PythonForDelphi\Demos\Demo04\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo05\__history
+rd /s /q PythonForDelphi\Demos\Demo05\__recovery
+rd /s /q PythonForDelphi\Demos\Demo05\Win64
+del /q PythonForDelphi\Demos\Demo05\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo05\Project1.identcache
+del /q PythonForDelphi\Demos\Demo05\Project1.stat
+del /q PythonForDelphi\Demos\Demo05\Project1.exe
+del /q PythonForDelphi\Demos\Demo05\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo06\__history
+rd /s /q PythonForDelphi\Demos\Demo06\__recovery
+rd /s /q PythonForDelphi\Demos\Demo06\Win64
+del /q PythonForDelphi\Demos\Demo06\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo06\Project1.identcache
+del /q PythonForDelphi\Demos\Demo06\Project1.stat
+del /q PythonForDelphi\Demos\Demo06\Project1.exe
+del /q PythonForDelphi\Demos\Demo06\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo07\__history
+rd /s /q PythonForDelphi\Demos\Demo07\__recovery
+rd /s /q PythonForDelphi\Demos\Demo07\Win64
+del /q PythonForDelphi\Demos\Demo07\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo07\Project1.identcache
+del /q PythonForDelphi\Demos\Demo07\Project1.stat
+del /q PythonForDelphi\Demos\Demo07\Project1.exe
+del /q PythonForDelphi\Demos\Demo07\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo08\__history
+rd /s /q PythonForDelphi\Demos\Demo08\__recovery
+rd /s /q PythonForDelphi\Demos\Demo08\Win64
+del /q PythonForDelphi\Demos\Demo08\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo08\Project1.identcache
+del /q PythonForDelphi\Demos\Demo08\Project1.stat
+del /q PythonForDelphi\Demos\Demo08\Project1.exe
+del /q PythonForDelphi\Demos\Demo08\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo09\__history
+rd /s /q PythonForDelphi\Demos\Demo09\__recovery
+rd /s /q PythonForDelphi\Demos\Demo09\Win64
+del /q PythonForDelphi\Demos\Demo09\ProjectGroup1.groupproj.local
+del /q PythonForDelphi\Demos\Demo09\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo09\Project1.identcache
+del /q PythonForDelphi\Demos\Demo09\Project1.stat
+del /q PythonForDelphi\Demos\Demo09\Project1.exe
+del /q PythonForDelphi\Demos\Demo09\Project1.rsm
+del /q PythonForDelphi\Demos\Demo09\demodll.dproj.local
+del /q PythonForDelphi\Demos\Demo09\demodll.identcache
+del /q PythonForDelphi\Demos\Demo09\demodll.stat
+del /q PythonForDelphi\Demos\Demo09\demodll.pyd
+del /q PythonForDelphi\Demos\Demo09\demodll.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo10_FireDAC\__history
+rd /s /q PythonForDelphi\Demos\Demo10_FireDAC\__recovery
+rd /s /q PythonForDelphi\Demos\Demo10_FireDAC\Win64
+del /q PythonForDelphi\Demos\Demo10_FireDAC\Demo10.dproj.local
+del /q PythonForDelphi\Demos\Demo10_FireDAC\Demo10.identcache
+del /q PythonForDelphi\Demos\Demo10_FireDAC\Demo10.stat
+del /q PythonForDelphi\Demos\Demo10_FireDAC\Demo10.exe
+del /q PythonForDelphi\Demos\Demo10_FireDAC\Demo10.rsm
+del /q PythonForDelphi\Demos\Demo10_FireDAC\Demo10_MemoryManager_EventLog.txt
+
+rd /s /q PythonForDelphi\Demos\Demo11\__history
+rd /s /q PythonForDelphi\Demos\Demo11\__recovery
+rd /s /q PythonForDelphi\Demos\Demo11\Win64
+del /q PythonForDelphi\Demos\Demo11\ThrdDemo.dproj.local
+del /q PythonForDelphi\Demos\Demo11\ThrdDemo.identcache
+del /q PythonForDelphi\Demos\Demo11\ThrdDemo.stat
+del /q PythonForDelphi\Demos\Demo11\ThrdDemo.exe
+del /q PythonForDelphi\Demos\Demo11\ThrdDemo.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo16\Example1\__history
+rd /s /q PythonForDelphi\Demos\Demo16\Example1\__recovery
+rd /s /q PythonForDelphi\Demos\Demo16\Example1\Win64
+del /q PythonForDelphi\Demos\Demo16\Example1\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo16\Example1\Project1.identcache
+del /q PythonForDelphi\Demos\Demo16\Example1\Project1.stat
+del /q PythonForDelphi\Demos\Demo16\Example1\Project1.exe
+del /q PythonForDelphi\Demos\Demo16\Example1\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo16\Example2\__history
+rd /s /q PythonForDelphi\Demos\Demo16\Example2\__recovery
+rd /s /q PythonForDelphi\Demos\Demo16\Example2\Win64
+del /q PythonForDelphi\Demos\Demo16\Example2\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo16\Example2\Project1.identcache
+del /q PythonForDelphi\Demos\Demo16\Example2\Project1.stat
+del /q PythonForDelphi\Demos\Demo16\Example2\Project1.exe
+del /q PythonForDelphi\Demos\Demo16\Example2\Project1.rsm
+
+rd /s /q PythonForDelphi\Demos\Demo17\__history
+rd /s /q PythonForDelphi\Demos\Demo17\__recovery
+rd /s /q PythonForDelphi\Demos\Demo17\Win64
+del /q PythonForDelphi\Demos\Demo17\Project1.dproj.local
+del /q PythonForDelphi\Demos\Demo17\Project1.identcache
+del /q PythonForDelphi\Demos\Demo17\Project1.stat
+del /q PythonForDelphi\Demos\Demo17\Project1.exe
+del /q PythonForDelphi\Demos\Demo17\Project1.rsm
+
+Pause
